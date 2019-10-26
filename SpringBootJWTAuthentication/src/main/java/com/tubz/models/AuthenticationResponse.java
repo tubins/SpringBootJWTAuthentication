1754adbbf117676f4f10b564f3b7580b42eae5e4
tubins <tubin.shaji@gmail.com>
2019-10-27 00:20:49 +0100

    Add AuthenticateRequest model
    Add AuthenticateRequest model to return the JWT to token to the client

---
+++ SpringBootJWTAuthentication/src/main/java/com/tubz/models/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package com.tubz.models;
+
+public class AuthenticationResponse {
+
+	private final String jwt;
+
+	public String getJwt() {
+		return jwt;
+	}
+
+	public AuthenticationResponse(String jwt) {
+		this.jwt = jwt;
+	}
+
+}
