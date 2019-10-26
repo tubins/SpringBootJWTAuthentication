ee7191ea704f12fd0b0a9008ca380d49e1bb2da3
tubins <tubin.shaji@gmail.com>
2019-10-26 22:45:13 +0100

    Disable password encoding

--- SpringBootJWTAuthentication/src/main/java/com/tubz/service/MyUserDetailsService.java
+++ SpringBootJWTAuthentication/src/main/java/com/tubz/service/MyUserDetailsService.java
@@ -2,10 +2,13 @@ package com.tubz.service;
 
 import java.util.ArrayList;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,5 +17,11 @@ public class MyUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
 		return new User("foo", "foo", new ArrayList<>());
+	}
+
+	@Bean
+	@SuppressWarnings("deprecation")
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 }
