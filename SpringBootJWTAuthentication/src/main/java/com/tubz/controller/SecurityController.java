ed5968054ba5728ee9bebdb47ca41b50f729fe75
tubins <tubin.shaji@gmail.com>
2019-10-27 00:59:57 +0100

    Fixup function placement in wrong class

--- SpringBootJWTAuthentication/src/main/java/com/tubz/controller/SecurityController.java
+++ SpringBootJWTAuthentication/src/main/java/com/tubz/controller/SecurityController.java
@@ -1,12 +1,16 @@
 package com.tubz.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.tubz.service.MyUserDetailsService;
 
+@SuppressWarnings("deprecation")
 @EnableWebSecurity
 public class SecurityController extends WebSecurityConfigurerAdapter {
 
@@ -16,5 +20,10 @@ public class SecurityController extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(myUserDetailsService);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 }
--- SpringBootJWTAuthentication/src/main/java/com/tubz/service/MyUserDetailsService.java
+++ SpringBootJWTAuthentication/src/main/java/com/tubz/service/MyUserDetailsService.java
@@ -2,13 +2,10 @@ package com.tubz.service;
 
 import java.util.ArrayList;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,11 +14,5 @@ public class MyUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
 		return new User("foo", "foo", new ArrayList<>());
-	}
-
-	@Bean
-	@SuppressWarnings("deprecation")
-	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
 	}
 }
