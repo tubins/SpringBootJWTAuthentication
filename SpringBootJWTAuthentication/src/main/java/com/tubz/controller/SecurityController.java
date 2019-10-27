dd6816673a1c6ab38f2b3ae9679fb0a9df8a8d8a
tubins <tubin.shaji@gmail.com>
2019-10-27 01:21:20 +0100

    Fixup AuthenticationManager bean creation issue
    Fix the backward compatibility issue of Spring-Boot2 with older version
    that cause the following issue. Error details given below.
    
    Description:
    
    Field authenticationManager in com.tubz.controller.HelloController
    required a bean of type
    'org.springframework.security.authentication.AuthenticationManager' that
    could not be found.
    
    The injection point has the following annotations:
    	- @org.springframework.beans.factory.annotation.Autowired(required=
    	true)
    
    
    Action:
    
    Consider defining a bean of type
    'org.springframework.security.authentication.AuthenticationManager' in
    your configuration.

--- SpringBootJWTAuthentication/src/main/java/com/tubz/controller/SecurityController.java
+++ SpringBootJWTAuthentication/src/main/java/com/tubz/controller/SecurityController.java
@@ -2,6 +2,7 @@ package com.tubz.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,12 +26,18 @@ public class SecurityController extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-			.authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated();
+		http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated();
 	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
 	}
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
 }
