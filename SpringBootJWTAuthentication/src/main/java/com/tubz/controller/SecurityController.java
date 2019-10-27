4bf0037d21fee106c481200bffef2197d79aee19
tubins <tubin.shaji@gmail.com>
2019-10-27 01:08:54 +0100

    Disable authentication
    Disable authentication of /authenticate end-point so than the clients
    can access this end-point to generate the JWT token using their username
    and password.

--- SpringBootJWTAuthentication/src/main/java/com/tubz/controller/SecurityController.java
+++ SpringBootJWTAuthentication/src/main/java/com/tubz/controller/SecurityController.java
@@ -3,6 +3,7 @@ package com.tubz.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -20,6 +21,12 @@ public class SecurityController extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(myUserDetailsService);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+			.authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated();
 	}
 
 	@Bean
